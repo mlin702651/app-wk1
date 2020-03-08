86d622efdba6859372bf51ff0b6eb9c9a4a6da99
Ding <mlin702651@gmail.com>
2020-03-08 18:24:45 +0800

    init

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>110719038</Text>
+      <Text>林巧華</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffe4b8',
+    alignItems: 'center',
+    justifyContent: 'center',
+  
+  },
+});
